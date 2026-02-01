26511301721123a268fccb56eedd7eab31ec61b0
Shashwat07s <153847258+Shashwat07s@users.noreply.github.com>
2026-02-01 20:12:55 +0530

    Update ProductLocators.ts

--- locators/ProductLocators.ts
+++ locators/ProductLocators.ts
@@ -1,10 +1,10 @@
 export class ProductLocators {
-  // Processor radio button (Slow)
+ 
   slowProcessor = '#product_attribute_75_5_31_96';
 
-  // Add to cart button
+
   addToCartButton = '#add-to-cart-button-75';
 
-  // Success message
+  
   successMessage = '.bar-notification.success';
 }
