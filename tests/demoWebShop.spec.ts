560771eab89aa191172ff5c780a5ba1989bcce48
Shashwat07s <153847258+Shashwat07s@users.noreply.github.com>
2026-02-01 20:12:12 +0530

    Update demoWebShop.spec.ts

--- tests/demoWebShop.spec.ts
+++ tests/demoWebShop.spec.ts
@@ -9,14 +9,14 @@ test('Demo Web Shop – End to End (Search Based)', async ({ page }) => {
 
   await page.goto('/');
 
-  // 1️⃣ Register
+  
   await new RegisterPage(page).registerUser();
   await captureScreenshot(page, '01_registration');
 
-  // 2️⃣ Search Computer → Select Simple Computer
+ 
   await new HomePage(page).searchAndSelectSimpleComputer();
 
-  // 3️⃣ Select Processor & Add to Cart
+
   const product = new ProductPage(page);
   await product.selectProcessorAndAddToCart();
 
@@ -24,7 +24,7 @@ test('Demo Web Shop – End to End (Search Based)', async ({ page }) => {
   expect(msg).toContain('The product has been added');
   await captureScreenshot(page, '02_product_added');
 
-  // 4️⃣ Cart → India → Terms → Checkout
+
   await new CartPage(page).checkout();
   await captureScreenshot(page, '03_checkout');
 });
